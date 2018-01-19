d096653fdf2a4fe102c04d8d5faa021d19d4d9e5
Samuel Zahrt <samuel.zahrt@warriorlife.net>
2018-01-19 11:17:06 -0800

    ADDED MONEY GENERATION
    we now have a source of income

--- webapp.js
+++ webapp.js
@@ -1,10 +1,12 @@
 // JavaScript Document
+// Give the user their income every 5 seconds
 var money = 1000;
 var h = 1;
 var roi = 100*h;
 
-
-function generate(m,roi) {
-	Math(m + roi);
-	setTimeout(generate(m,roi), 5000);
+function generate() {
+	money += roi;
+	alert(money);
 }
+
+window.onload = setInterval(generate, 5000);
