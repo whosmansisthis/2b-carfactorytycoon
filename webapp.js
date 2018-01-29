e4ac5269cb4f529d0d7f40ef75a6fc4ef073c106
Samuel Zahrt <samuel.zahrt@warriorlife.net>
2018-01-29 11:29:46 -0800

    tried and kinda did upgrased work

--- webapp.js
+++ webapp.js
@@ -10,19 +10,15 @@ function generate() {
 function research_car() {
 	research = research * 1.1;
 	money-=100;
+	e(money);
 }
 
+// update the value stored in javascript onto the page.
 function e(val) {
 	document.getElementById("d").value = val;
 }
  
- function v() {
-	 try {
-		 d(eval(document.getElementById("d").value));
-	 }
-	 catch(err) {
-		 d("Error");
-	 }
- }
+
  
 window.onload = setInterval(generate, 5000);
+window.onload = setInterval(e(money), 005);
