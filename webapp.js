c3d1a03d4b5403e4ee91a5e43864fc615bf52ab9
Samuel Zahrt <samuel.zahrt@warriorlife.net>
2018-01-31 11:48:10 -0800

    did stuff
    fixed money and made it auto update
    
    am adding cookies(i need to find a way to load em)

--- webapp.js
+++ webapp.js
@@ -5,20 +5,42 @@ var research = 1;
 
 function generate() {
 	money += 100 * research;
+	e(money);
 }
 
 function research_car() {
-	research = research * 1.1;
-	money-=100;
+	research = research * 1.1
+	e(money);
+	money - 1000;
 	e(money);
 }
 
 // update the value stored in javascript onto the page.
 function e(val) {
 	document.getElementById("d").value = val;
+	var money = val.toFixed(2)
+	e(money);
+	setCookie()
 }
  
-
  
 window.onload = setInterval(generate, 5000);
-window.onload = setInterval(e(money), 005);
+
+// COOKIE	
+
+function setCookie() {
+    var exdays = 365;
+	var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = "money=" + money + ";" + expires;
+    document.cookie = "=research" + research + ";" + expires;
+}
+
+function loadCookies() {
+    var string = document.cookie;
+	// split("; ");
+	// split("=");
+	// key_value_pair[0];
+	alert(string);
+}
